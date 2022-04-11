4ef1d82edaf37164a8b44b408e6b7f16fe25b024
Trueqo <julianochoa1999@gmail.com>
2022-04-11 10:49:16 -0500

    home completa

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,8 +4,12 @@ import FrontCard from '../Cards/FrontCard'
 import css from './Card.css'
 
 const Card = () => {
+  function girar (){
+    const card = document.getElementById('card')
+    card.classList.toggle('active')
+  }
   return (
-    <div className='Card absolute min-w-full min-h-full rounded-full active'>
+    <div className='Card absolute min-w-full min-h-full rounded-full ' id='card' onClick={girar}>
     <FrontCard/>
     <BackCard/>
     </div>
