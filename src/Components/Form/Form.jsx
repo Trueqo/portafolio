019af2147327e43e6341ad1b811b496e6ca666c9
Trueqo <julianochoa1999@gmail.com>
2022-04-11 17:39:15 -0500

    contact me finished

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const Form = () => {
   return (
-    <div className='relative justify-center items-center text-center'>
-        <span className='text-4xl font-bold text-color1'>CONTACT ME !</span>
-        <div className='bg-white w-full h-[30rem] rounded-2xl mt-5'>
-        </div>
-        
+    <div className="relative justify-center items-center text-center">
+      <span className="text-4xl font-bold text-color1">CONTACT ME !</span>
+      <form className="flex flex-col bg-white  h-[20rem] rounded-2xl mt-5 shadow-md px-10 md:px-[2rem] justify-center gap-5  relative md:w-96">
+        <input
+          type='Text'
+          placeholder="Name"
+          className="border-2 border-color2 rounded-md pl-2"
+        ></input>
+        <input
+          type='Text'
+          placeholder="Company"
+          className="border-2 border-color2 rounded-md pl-2"
+        ></input>
+        <input
+          type="number"
+          placeholder="Name"
+          className="border-2 border-color2 rounded-md pl-2 text-color1"
+        ></input>
+        <textarea
+          placeholder="additional message"
+          className="border-2 border-color2 rounded-md pl-2"
+        ></textarea>
+        <input
+          type='submit'
+          className="w-[6rem] h-[1.9rem] absolute bottom-[-2.5rem]  bg-color1 text-white rounded-xl" 
+        ></input>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
--- src/Views/Contact.jsx
+++ src/Views/Contact.jsx
@@ -3,10 +3,10 @@ import Form from '../Components/Form/Form'
 import Recurso from '../assets/Recurso.png'
 const Contact = () => {
   return (
-    <div className='min-h-screen px-10 pt-32 pb-44 relative flex flex-col'>
+    <div className='min-h-screen px-10  pt-32 pb-44 relative flex flex-col items-center'>
             <Form/>
             <div className='absolute bottom-[-0.4rem] right-0'>
-              <img src={Recurso} className='object-fill w-56 md:w-96'/>
+              <img src={Recurso} className='object-fill w-56 md:w-80 lg:w-80'/>
             </div>
     </div>
   )
