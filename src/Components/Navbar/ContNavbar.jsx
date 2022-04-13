222e48b166a2221a76bc6cfd838566d329929f49
Trueqo <julianochoa1999@gmail.com>
2022-04-13 17:14:56 -0500

    the navbar styles are organized

--- src/Components/Navbar/ContNavbar.jsx
+++ src/Components/Navbar/ContNavbar.jsx
@@ -6,28 +6,28 @@ const ContNavbar = ({showMenu,active}) => {
         <ul className={active ? 'flex flex-col items-center md:hidden justify-center left-10  fixed inset-0 bg-color1 border-2 border-black z-10 ':'hidden'}>
 
                     <button className='absolute top-1 right-10 mt-3 '><AiFillCloseSquare onClick={showMenu} className='cursor-pointer w-10 h-10 text-white' /></button>
-                    <li className='hover:border-b-2 hover:border-teal-900 duration-500 cursor-pointer '>
-                        <a href='#' className='text-xl hover:text-teal-900 duration-500'>
+                    <li className='hover:border-b-2 hover:border-teal-900 duration-500 cursor-pointer  mb-20'>
+                        <a href='#' className='text-white  text-5xl hover:text-teal-900 duration-500'>
                             Home
                         </a>
                     </li>
-                    <li className='hover:border-b-2 hover:border-teal-900 duration-500 cursor-pointer '>
-                        <a href='#' className='text-xl hover:text-teal-900 duration-500'>
+                    <li className='hover:border-b-2 hover:border-teal-900 duration-500 cursor-pointer  mb-20'>
+                        <a href='#' className='text-white  text-5xl hover:text-teal-900 duration-500'>
                             Projects
                         </a>
                     </li>
-                    <li className='hover:border-b-2 hover:border-teal-900 duration-500 cursor-pointer'>
-                        <a href='#' className='text-xl hover:text-teal-900 duration-500'>
+                    <li className='hover:border-b-2 hover:border-teal-900 duration-500 cursor-pointer mb-20'>
+                        <a href='#' className='text-white  text-5xl hover:text-teal-900 duration-500'>
                             Achievements
                         </a>
                     </li>
-                    <li className='hover:border-b-2 hover:border-teal-900 duration-500 cursor-pointer'>
-                        <a href='#' className='text-xl hover:text-teal-900 duration-500'>
+                    <li className='hover:border-b-2 hover:border-teal-900 duration-500 cursor-pointer mb-20'>
+                        <a href='#' className='text-white  text-5xl hover:text-teal-900 duration-500'>
                             Contact
                         </a>
                     </li>
-                    <li className='hover:border-b-2 hover:border-white duration-500 cursor-pointer border-2 border-color2 rounded-xl  bg-color2 '>
-                        <a href='#' className='text-white  text-xl hover:text-black duration-500 mx-4'>
+                    <li className='hover:border-b-2 hover:border-white duration-500 cursor-pointer border-2 border-color2 rounded-xl  bg-color2  mb-20'>
+                        <a href='#' className='text-white   text-5xl hover:text-black duration-500 mx-4'>
                             CV
                         </a>
                     </li>   
