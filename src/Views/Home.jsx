55da1d99d0831df32baf5afe473eab08d2471613
Trueqo <julianochoa1999@gmail.com>
2022-04-14 23:53:16 -0500

    fixed home

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -13,13 +13,13 @@ const Home = () => {
   return (
     <div className='min-h-screen text-center flex flex-col items-center'>
       <span className='text-5xl font-bold text-white mt-4 font-akshar'>Julián Ochoa Barco</span>
-      <span className='text-2xl font-bold text-black mt-4 font-akshar'>FULL-STACK WEB DEVELOPER</span>
+      <span className='text-3xl font-bold text-black mt-4 font-akshar'>FULL-STACK WEB DEVELOPER</span>
       <ContainerCard/>
       <div className='text-center flex flex-col'>
-      <span className='font-bold mt-5'>Hello !</span>
-      <span>Wellcome to my web portfolio.</span>
-      <div className='w-72 h-44 text-left'>
-      <span>Let me tell you about me. I am Julián OchoaBarco. I am 22 year old. I live in Medellin-Colombia. I am a Full-Stack developer and I am currently studying Mechatronics Engineering. I am engaged</span>
+      <span className='font-akshar font-bold text-3xl mt-5'>Hello !</span>
+      
+      <div className='px-[27rem] h-44 text-left text-black text-xl'>
+      <span>Wellcome to my web portfolio. Let me tell you about me. I am Julián OchoaBarco. I am 22 year old. I live in Medellin-Colombia. I am a Full-Stack developer and I am currently studying Mechatronics Engineering. I am engaged</span>
       </div>
       </div>
       <div className='h-auto w-full flex flex-col'>
