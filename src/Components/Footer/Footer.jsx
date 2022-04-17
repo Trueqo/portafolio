70bd9d30b2300494bbd35f15056986d20ddb8326
Trueqo <julianochoa1999@gmail.com>
2022-04-17 00:37:14 -0500

    button wsp added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,6 +3,10 @@ import { AiFillGithub } from "react-icons/ai";
 import { AiOutlineWhatsApp } from "react-icons/ai";
 import {IoIosRocket} from "react-icons/io"
 const Footer = () => {
+
+  const linkWSP = 'https://chatwith.io/es/s/625ba34dd1135'
+  const linkWSP2 = 'https://wa.me/573017604450?text=%20i%20love%20you%20my%20honney'
+
   return (
     <div className="h-[10rem] w-full  bottom-0  text-white  flex flex-row  bg-color1 items-center justify-between">
       <div className=" text-xl ml-4 md:ml-10 lg:ml-16 md:text-[1.7rem] lg:text-4xl flex md:gap-1 lg:gap-4 flex-col md:flex-row">
@@ -12,7 +16,7 @@ const Footer = () => {
       </div>
       <div className="mr-2 text-7xl flex flex-row md:gap-5 md:mr-10">
         <a className="hover:text-violet-800"> <AiFillGithub /></a>
-        <a className="hover:text-green-500"><AiOutlineWhatsApp /></a>
+        <a  href={linkWSP2} className="hover:text-green-500"><AiOutlineWhatsApp /></a>
       </div>
     </div>
   );
